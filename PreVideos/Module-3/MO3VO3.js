d839da45e9041ec9621ff3cfcd6a6696e0f3e2d3
Erenshoyo <asiftauhid4665@gmail.com>
2026-05-12 12:42:55 +0600

    Task: Stack implementation using Array

---
+++ PreVideos/Module-3/MO3VO3.js
@@ -0,0 +1,48 @@
+//? Stack -> LIFO - Last In First Out
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(value) {
+    this.items.push(value);
+  }
+
+  pop() {
+    if (this.isEmpty()) return undefined;
+
+    this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) return undefined;
+
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const stack = new Stack();
+
+console.log(stack.peek());
+console.log(stack.isEmpty());
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+stack.print();
+console.log(stack.peek());
+
+console.log(stack.pop());
+
+stack.print();
+console.log(stack.peek());
