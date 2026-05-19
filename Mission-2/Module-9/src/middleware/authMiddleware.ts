56b20e92d33e983287c12ff433eaee926051be4b
Erenshoyo <asiftauhid4665@gmail.com>
2026-05-20 00:10:56 +0600

    Task: facing difficulties understanding middleware

--- Mission-2/Module-9/src/middleware/authMiddleware.ts
+++ Mission-2/Module-9/src/middleware/authMiddleware.ts
@@ -1,33 +1,64 @@
 import dotnet from "dotenv";
 import type { NextFunction, Request, Response } from "express";
-import jwt, { type JwtPayload } from "jsonwebtoken";
+import jwt, { decode, type JwtPayload } from "jsonwebtoken";
+import { pool } from "../db";
 
 dotnet.config();
 
 //* Custom middleware
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    // console.log("This is a protected Route");
+    // Using try catch so that the whole server doesn't crash incase of an unidentified error.
+    try {
+      // console.log("This is a protected Route");
 
-    //? Authorization.
-    const token = req.headers.authorization;
+      //? Authorization.
+      const token = req.headers.authorization;
 
-    if (!token) {
-      res.status(401).json({
-        success: false,
-        message: "Unauthorized",
-      });
+      if (!token) {
+        res.status(401).json({
+          success: false,
+          message: "Unauthorized",
+        });
+      }
+      // console.log(token);
+
+      const decoded = jwt.verify(
+        token as string,
+        process.env.JWT_SECRET as string,
+      ) as JwtPayload;
+
+      const userData = await pool.query(
+        `
+      SELECT * FROM users WHERE email=$1
+      `,
+        [decoded.email],
+      );
+
+      console.log(userData);
+
+      const user = userData.rows[0];
+      // console.log(user);
+
+      if (userData.rows.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: "User not found.",
+        });
+      }
+
+      if (user.is_active === false) {
+        res.status(403).json({
+          success: false,
+          message: "Forbidden.",
+        });
+      }
+
+      req.user = decoded; // req : { user: {} }
+      next();
+    } catch (error) {
+      next(error);
     }
-    console.log(token);
-
-    const decoded = jwt.verify(
-      token as string,
-      process.env.JWT_SECRET as string,
-    ) as JwtPayload;
-
-    console.log(decoded);
-
-    next();
   };
 };
 
