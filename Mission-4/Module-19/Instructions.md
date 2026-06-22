0b4de7207653c2f3d1900d8501ee608cb92f35f6
Erenshoyo <asiftauhid4665@gmail.com>
2026-06-22 12:41:17 +0600

    Started a backend project using Prisma ORM

---
+++ Mission-4/Module-19/Instructions.md
@@ -0,0 +1,5 @@
+## Instruction for Module-19
+
+-Followed the official Prisma Documentation alongside the Module Code.
+
+*This modeule have the same code as Module-18*
---
+++ Mission-4/Module-20/src/app.ts
@@ -0,0 +1,29 @@
+import cookieParser from "cookie-parser";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+import { userRoutes } from "./modules/user/user.route";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true, // for some work related to cookie and token.
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", async (req: Request, res: Response) => {
+  // const user = await prisma.user.findMany()
+  // console.log(user);
+  res.send("Prisma Press server working!!!");
+});
+
+// app.post();
+app.use("/api/users", userRoutes)
+
+export default app;
