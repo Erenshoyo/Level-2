042cf2865087eec54fffc4fcb6b85864232bbd86
Erenshoyo <asiftauhid4665@gmail.com>
2026-07-05 22:53:36 +0600

    Learned about transaction and rollback

--- Mission-4/Module-22/src/modules/post/post.service.ts
+++ Mission-4/Module-22/src/modules/post/post.service.ts
@@ -34,52 +34,93 @@ const getPostById = async (postId: string) => {
   //   },
   // });
 
-  await prisma.post.update({
-    where: {
-      id: postId,
-    },
-    data: {
-      views: {
-        increment: 1,
+  // await prisma.post.update({
+  //   where: {
+  //     id: postId,
+  //   },
+  //   data: {
+  //     views: {
+  //       increment: 1,
+  //     },
+  //   },
+  //   // include: {
+  //   //   author: {
+  //   //     omit: {
+  //   //       password: true,
+  //   //     },
+  //   //   },
+  //   //   comments: true,
+  //   // },
+  // });
+
+  // //throw new Error("Throw fake Error");
+
+  // const post = await prisma.post.findUniqueOrThrow({
+  //   where: {
+  //     id: postId,
+  //   },
+  //   include: {
+  //     author: {
+  //       omit: { password: true },
+  //     },
+  //     comments: {
+  //       where: {
+  //         status: CommentStatus.APPROVED,
+  //       },
+  //       // orderBy: {
+  //       //   createdAt: "desc"
+  //       // }
+  //     },
+  //     _count: {
+  //       select: {
+  //         comments: true,
+  //       },
+  //     },
+  //   },
+  // });
+
+  // return post;
+
+  const transactionResult = await prisma.$transaction(async (tx) => {
+    await tx.post.update({
+      where: {
+        id: postId,
       },
-    },
-    // include: {
-    //   author: {
-    //     omit: {
-    //       password: true,
-    //     },
-    //   },
-    //   comments: true,
-    // },
+      data: {
+        views: {
+          increment: 1,
+        },
+      },
+    });
+
+    const post = await tx.post.findUniqueOrThrow({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: {
+          omit: { password: true },
+        },
+        comments: {
+          where: {
+            status: CommentStatus.APPROVED,
+          },
+          // orderBy: {
+          //   createdAt: "desc"
+          // }
+        },
+        _count: {
+          select: {
+            comments: true,
+          },
+        },
+      },
+    });
+
+    return post;
   });
 
-  //throw new Error("Throw fake Error");
-
-  const post = await prisma.post.findUniqueOrThrow({
-    where: {
-      id: postId,
-    },
-    include: {
-      author: {
-        omit: { password: true },
-      },
-      comments: {
-        where: {
-          status: CommentStatus.APPROVED,
-        },
-        // orderBy: {
-        //   createdAt: "desc"
-        // }
-      },
-      _count: {
-        select: {
-          comments: true,
-        },
-      },
-    },
-  });
-
-  return post;
+  return transactionResult;
 };
 
 const updatePost = async (
